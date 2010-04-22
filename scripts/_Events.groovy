08db9efc2db2147600896fd073a2d06636d9c038
Mike Wille <mike.wille@flowz.com>
2010-04-22 09:50:03 -0400

    Added block for OS specific details.  Prevent errors on non OS X systems.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,11 +1,15 @@
 
 eventStatusFinal = { msg ->
-    String.metaClass.say = {
-        ["osascript", "-e", "tell application \"iTunes\" to set sound volume to 30"].execute()
-   	    sleep(300)
-        "say \"${delegate}\"".execute().waitFor()
-   	    ["osascript", "-e", "tell application \"iTunes\" to set sound volume to 100"].execute()
-    }
+    if(System.properties["os.name"] == "Mac OS X") {
+		String.metaClass.say = {
+    	    ["osascript", "-e", "tell application \"iTunes\" to set sound volume to 30"].execute()
+   	    	sleep(300)
+	        "say \"${delegate}\"".execute().waitFor()
+   		    ["osascript", "-e", "tell application \"iTunes\" to set sound volume to 100"].execute()
+	    }
+	} else {
+		String.metaClass.say = { }
+	}
 
     if(msg.startsWith("Server running.")) {
    	    "Application is running".say()
